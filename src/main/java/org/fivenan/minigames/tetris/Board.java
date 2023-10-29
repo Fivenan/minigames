205f45473d8edde1eb75c9d08cbc4ae3862207c4
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-10-29 14:15:22 +0100

    shape update

--- src/main/java/org/fivenan/minigames/tetris/Board.java
+++ src/main/java/org/fivenan/minigames/tetris/Board.java
@@ -6,10 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Timer;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import org.fivenan.minigames.tetris.Shape.Tetrominoe;
 
